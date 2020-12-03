1e7eb70a5ae2443a5e7d3260787cc4812d0297c9
Viren Joopelli <vjoopelli@it-usa-vx0182.home>
2020-12-03 00:27:02 -0600

    first

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
+import utilStyles from '../styles/utils.module.css'
+import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps(){
+	const allPostsData = getSortedPostsData()
+		return {
+			props: {
+				allPostsData
+			}
+		}	
+}
+
+export default function Home({allPostsData}) {
+  return (
+   <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I’m currently an Associate Product Manager on the Moderation Experience team at Indeed, building tools for fraud prevention and employer veritfication. Before that, I studied economics and philosophy at UT Austin. Outside of work, I spend time reading and experimenting with digital and generative art, among other things.</p>
+        <p>I’m building this site as a space for myself first - to explore new ideas, cultivate openness, and practice vulnerability. If you’re interested in chatting, feel free to reach out via e-mail or DM me on Twitter. I’m happy to talk about anything.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+				  <Link href={`/posts/${id}`}>
+				    <a>{title}</a>
+				  </Link>
+				  <br />
+				  <small className={utilStyles.lightText}>
+				    <Date dateString={date} />
+				  </small>
+				</li>
+          ))}
+        </ul>
+      </section>
+
+	</Layout>
+  )
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Link from 'next/Link'
+import Head from 'next/Head'
+import Layout from '../../components/layout'
+
+export default function FirstPost(){
+	return(
+		<Layout>
+			<Head>
+				<title>First Post</title>
+			</Head>
+			<h1>First Post</h1>
+			<h2>
+				<Link href = "/">
+					<a>Back to home</a>
+				</Link>
+			</h2>
+		</Layout>
+	)
+}
