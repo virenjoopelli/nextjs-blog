26991ee22d0d8d7d58a4a47acbfafd3c8c8b17a7
Viren Joopelli <vjoopelli@it-usa-vx0182.home>
2020-12-24 15:39:21 -0600

    fixing bug

--- components/layout.js
+++ components/layout.js
@@ -16,7 +16,8 @@ export default function Layout({ children, home }) {
           content="Learn how to build a personal website using Next.js"
         />
         <meta
-          property="og:image"g
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
